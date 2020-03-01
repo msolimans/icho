67d0071463831032fa5d354901a9cb979864488c
Muhammad Soliman <m.solimanz@hotmail.com>
2020-02-29 23:01:07 -0500

    grpc g/w implementation

---
+++ icho/ichp.pb.imp.go
@@ -0,0 +1,33 @@
+package icho
+
+import (
+	. "context"
+	"github.com/golang/protobuf/ptypes/empty"
+)
+
+type IchoServerImp struct {
+}
+
+func NewIchoServerImp() *IchoServerImp {
+	return &IchoServerImp{}
+}
+
+func (i *IchoServerImp) GetEcho(ctx Context, req *GetEchoRequest) (*PostEchoResponse, error) {
+	return echo(req.GetText())
+}
+
+func (i *IchoServerImp) GetHealth(ctx Context, req *empty.Empty) (*HealthResponse, error) {
+	return &HealthResponse{Status: "ok"}, nil
+}
+
+func (i *IchoServerImp) PostEcho(ctx Context, req *PostEchoRequest) (*PostEchoResponse, error) {
+	return echo(req.GetText())
+}
+
+func echo(str ...string) (*PostEchoResponse, error) {
+	if len(str) > 0 && str[0] != "" {
+		return &PostEchoResponse{Response: str[0]}, nil
+	}
+
+	return &PostEchoResponse{Response: "hello world"}, nil
+}
